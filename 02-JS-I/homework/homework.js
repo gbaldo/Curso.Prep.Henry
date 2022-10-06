e0c82c98b4c03b16c37515e5139373a91234f4d4
unknown <gonzalobalo1991@gmail.comgit config --global user.name Gonzalo>
2022-10-05 21:46:37 -0300

    32 ejercicios listos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,5 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
+const { stringify } = require("git-url-parse");
+
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = 'Hola';
 
@@ -220,15 +222,15 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+  var resultado;
+  resultado= false;
   if (numero>0){
-    console.log('Es positivo');
-  }  
-   else if (numero<0){
-    console.log('Es negativo');
+    resultado= 'Es positivo';
   }
-   else if (numero=0){
-    return false;
+  else if (numero <0){
+    resultado= 'Es negativo';
   }
+  return resultado;
 }
 
 
@@ -254,6 +256,10 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var nueva;
+  nueva= 'Hola ';
+  nueva= nueva + nombre + '!';
+  return nueva;
   
   
 }
@@ -261,6 +267,9 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  var area;
+  area= alto*ancho;
+  return area;
   
 }
 
@@ -268,14 +277,18 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perimetro;
+  perimetro= lado*4;
+  return perimetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  var area;
+  area= (base*altura)/2;
+  return area;
 }
 
 
@@ -283,6 +296,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  var dolar;
+  dolar= euro*1.2;
+  return dolar;
   
 }
 
@@ -293,7 +309,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length > 2){
+    console.log ('Dato incorrecto');
+  }
+  else {if (letra=='a'||'e'||'i'||'o'||'u'){
+    console.log("Es vocal!");
+  }else {
+    console.log("Dato incorrecto");
+  }
+}
 }
 
 
